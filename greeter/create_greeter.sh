44c4770b5f237b4149ee79c2fe2fe9f36caa76ba
Ramon Miret <ramonet.miret@gmail.com>
2025-11-09 13:40:38 +0100

    Creado el greeter

---
+++ greeter/create_greeter.sh
@@ -0,0 +1,4 @@
+# pacman -Qs sddm
+
+sudo cp -r /home/$USER/.config/sddm/minesddm/ /usr/share/sddm/themes/
+sudo ln -s /home/$USER/.config/sddm/sddm.conf /etc/sddm.conf
--- initialize_config.sh
+++ initialize_config.sh
@@ -1,8 +1,10 @@
 # ADD THE ELIMITION OF ALL THE OTHERS REPOSITORIES IF THEY EXIST PREVIOUSLY
 stow browser
-stow hyprland
 stow nvim
 stow obsidian
 stow keyboard
 ./keyboard/.config/setlayout.sh
+
+rm ~/.bashrc
+stow general_configuration
 
