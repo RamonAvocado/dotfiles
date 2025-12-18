95ff988027b7c3463c3a6ef0f1ca80cdeb14ca21
RamonAvocado <ramonet.miret@gmail.com>
2025-12-18 07:33:54 +0100

    Copied keymaps from LazyVim that i find utils

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,13 +1,30 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Exit the current file' }) -- EXIT THE CURRENT FILE
-vim.keymap.set('n', '<leader>q', '<cmd>:q<CR>', { desc = 'Close window' }) -- EXIT THE CURRENT FILE
+map = vim.keymap.set
+----------------------KEYMAPS OF LAZYVIM-------------------
+-- better up/down
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv") -- TO MOVE AROUND THE LINE/S YOU HAVE SELECTED
+-- better indenting
+map("x", "<", "<gv")
+map("x", ">", ">gv")
+
+-- quit
+map("n", "<leader>qQ", "<cmd>qa<cr>", { desc = "Quit All" })
+-----------------------------------------------------------
+
+
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Exit the current file' }) -- EXIT THE CURRENT FILE
+vim.keymap.set('n', '<leader>qq', '<cmd>:q<CR>', { desc = 'Close window' })       -- EXIT THE CURRENT FILE
+
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")                                      -- TO MOVE AROUND THE LINE/S YOU HAVE SELECTED
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
-vim.keymap.set('n', 'J', 'mzJ`z') -- KEEP THE CURSOR IN PLACE WHEN YOU BRING LINE UP
+vim.keymap.set('n', 'J', 'mzJ`z')                                           -- KEEP THE CURSOR IN PLACE WHEN YOU BRING LINE UP
 
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Move down half a page' }) -- TO KEEP THE CURSOR CENTERED WHEN YOU MOVE AROUND HALF PAGES
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Move up half a page' })
@@ -37,9 +54,11 @@ vim.keymap.set('i', 'ß', '<Esc>', { noremap = true, silent = true }) -- TO EXIT
 vim.keymap.set('n', '<leader>f', vim.lsp.buf.format) -- TO FORMAT THE CODE
 
 -- vim.keymap.set('n', 'grnf', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Replace all the word that im standing on' }) -- TO REPLACE ALL THE WORD NAMED THE SAME AS THE ONE IM STANDING
-vim.keymap.set('n', 'grnf', [[:.,$s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Replace word under cursor from this line downward' }) -- TO WORD NAMED THE SAME AS THE ONE IM STANDING ONWARDS
+vim.keymap.set('n', 'grnf', [[:.,$s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+  { desc = 'Replace word under cursor from this line downward' }) -- TO WORD NAMED THE SAME AS THE ONE IM STANDING ONWARDS
 -- vim.keymap.set('x', 'grnf', [["sy:%s/<C-r>s/<C-r>s/gI<Left><Left><Left>]], { desc = 'Replace all occurrences of selected text' })
-vim.keymap.set('x', 'grnf', [["sy:'<,$s/<C-r>s/<C-r>s/gI<Left><Left><Left>]], { desc = 'Replace selected text from selection start line downward' })
+vim.keymap.set('x', 'grnf', [["sy:'<,$s/<C-r>s/<C-r>s/gI<Left><Left><Left>]],
+  { desc = 'Replace selected text from selection start line downward' })
 
 vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true, desc = 'Makes the current file executable' }) -- TO MAKE SCRIPS EXECUTABLE
 
