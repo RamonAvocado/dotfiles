642e87bbe8738ca7d59601b2ea5d0c4f76e9a6a8
Ramon Miret <ramonet.miret@gmail.com>
2025-12-21 19:48:55 +0100

    Created a backup for the config

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -44,16 +44,55 @@ return {
     },
 
     -- Document existing key chains
+    -- spec = {
+    --   { '<leader>s',  group = '[S]earch' },
+    --   { '<leader>t',  group = '[T]oggle' },
+    --   { '<leader>g',  group = '[G]it',             mode = { 'n', 'v' } },
+    --   { '<leader>sg', group = '[S]earch [G]it',    mode = { 'n', 'v' } },
+    --   { '<leader>o',  group = '[O]pen' },
+    --   { 'go',         group = 'LSP [O]pen Symbols' },
+    --   { 'gr',         group = 'LSP Utilities' },
+    -- },
     spec = {
-      { '<leader>s',  group = '[S]earch' },
-      { '<leader>t',  group = '[T]oggle' },
-      { '<leader>g',  group = '[G]it',             mode = { 'n', 'v' } },
-      { '<leader>sg', group = '[S]earch [G]it',    mode = { 'n', 'v' } },
-      { '<leader>o',  group = '[O]pen' },
-      { 'go',         group = 'LSP [O]pen Symbols' },
-      { 'gr',         group = 'LSP Utilities' },
+      {
+        mode = { "n", "x" },
+        { "<leader><tab>", group = "tabs" },
+        { "<leader>c",     group = "code" },
+        { "<leader>d",     group = "debug" },
+        { "<leader>dp",    group = "profiler" },
+        { "<leader>f",     group = "file/find" },
+        { "<leader>g",     group = "git" },
+        { "<leader>gh",    group = "hunks" },
+        { "<leader>q",     group = "quit/session" },
+        { "<leader>s",     group = "search" },
+        { "<leader>u",     group = "ui" },
+        { "<leader>x",     group = "diagnostics/quickfix" },
+        { "[",             group = "prev" },
+        { "]",             group = "next" },
+        { "g",             group = "goto" },
+        { "gs",            group = "surround" },
+        { "z",             group = "fold" },
+        {
+          "<leader>b",
+          group = "buffer",
+          expand = function()
+            return require("which-key.extras").expand.buf()
+          end,
+        },
+        {
+          "<leader>w",
+          group = "windows",
+          proxy = "<c-w>",
+          expand = function()
+            return require("which-key.extras").expand.win()
+          end,
+        },
+        -- better descriptions
+        { "gx", desc = "Open with system app" },
+      },
     },
-  },
+    --
+  }
 }
 
 -- {
