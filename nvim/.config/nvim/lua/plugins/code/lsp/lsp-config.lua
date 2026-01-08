06e513caba3f98b4909f1b52da0a0c113532f1ab
RamonAvocado <ramonet.miret@gmail.com>
2026-01-08 07:45:32 +0100

    LSP: Added Ruff as a server, and added a example of working config for inspiration

--- nvim/.config/nvim/lua/plugins/code/lsp/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/code/lsp/lsp-config.lua
@@ -77,11 +77,13 @@ return {
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-      bashls = {},
-      marksman = {},
+      -- bashls = {},
+      -- marksman = {},
       -- clangd = {},
       -- gopls = {},
       -- pyright = {},
+			-- TODO: ADD all the other servers
+      ruff = {},
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
