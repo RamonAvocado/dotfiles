9611605d0006ed7850e6c1d59c2a590d2099e608
RamonAvocado <ramonet.miret@gmail.com>
2026-01-10 12:32:25 +0100

    Nvim: Some test with the LSP, to better understand it

--- nvim/.config/nvim/lua/config/keymaps/lsp/lsp-config.lua
+++ nvim/.config/nvim/lua/config/keymaps/lsp/lsp-config.lua
@@ -1,54 +1,52 @@
--- NOTE: Remember that Lua is a real programming language, and as such it is possible
--- to define small helper and utility functions so you don't have to repeat yourself.
---
--- In this case, we create a function that lets us more easily define mappings specific
--- for LSP related items. It sets the mode, buffer and description for us each time.
-
 local M = {}
 
 function M.setup(event)
-	local map = function(keys, func, desc, mode)
-		mode = mode or 'n'
-		vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-	end
+  local map = function(keys, func, desc, mode)
+    mode = mode or 'n'
+    vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+  end
 
-	-- Rename the variable under your cursor.
-	--  Most Language Servers support renaming across files, etc.
-	map('grnn', vim.lsp.buf.rename, '[R]e[n]ame')
+  map('<leader>ccc', "<cmd>:lua print('ha') <CR>", '[L]ist [C]apabilities', 'n')
+  -- map(
+  --   '<leader>cL',
+  --   "<cmd>:lua for _,c in ipairs(vim.lsp.get_clients({bufnr=0})) do print(c.name); print(vim.inspect(c.server_capabilities)); print('---') end<CR>",
+  --   '[L]ist [C]apabilities'
+  -- )
+  -- Rename the variable under your cursor.
+  --  Most Language Servers support renaming across files, etc.
+  map('grnn', vim.lsp.buf.rename, '[R]e[n]ame')
 
-	-- Execute a code action, usually your cursor needs to be on top of an error
-	-- or a suggestion from your LSP for this to activate.
-	map('gra', vim.lsp.buf.code_action, '[G]oto [C]ode [A]ction', { 'n', 'x' })
-	map('gh', vim.lsp.buf.hover, '[G]oto [H]over')
+  -- Execute a code action, usually your cursor needs to be on top of an error
+  -- or a suggestion from your LSP for this to activate.
+  map('gra', vim.lsp.buf.code_action, '[G]oto [C]ode [A]ction', { 'n', 'x' })
+  map('grh', vim.lsp.buf.hover, '[G]oto [H]over') -- "K"
 
-	-- Find references for the word under your cursor.
-	map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]efe[r]ences')
+  -- Find references for the word under your cursor.
+  map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]efe[r]ences')
 
-	-- Jump to the implementation of the word under your cursor.
-	--  Useful when your language has ways of declaring types without an actual implementation.
-	map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+  -- Jump to the implementation of the word under your cursor.
+  --  Useful when your language has ways of declaring types without an actual implementation.
+  map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+	
+  -- Jump to the type of the word under your cursor.
+  --  Useful when you're not sure what type a variable is and you want to see
+  --  the definition of its *type*, not where it was *defined*.
+  map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 
-	-- Jump to the definition of the word under your cursor.
-	--  This is where a variable was first declared, or where a function is defined, etc.
-	--  To jump back, press <C-t>.
-	map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+  -- Jump to the definition of the word under your cursor.
+  --  This is where a variable was first declared, or where a function is defined, etc.
+  --  To jump back, press <C-t>.
+  map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+  map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-	-- WARN: This is not Goto Definition, this is Goto Declaration.
-	--  For example, in C this would take you to the header.
-	map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  -- Fuzzy find all the symbols in your current document.
+  --  Symbols are things like variables, functions, types, etc.
+  map('gof', require('telescope.builtin').lsp_document_symbols, '[O]pen [F]ile Symbols')
 
-	-- Fuzzy find all the symbols in your current document.
-	--  Symbols are things like variables, functions, types, etc.
-	map('gof', require('telescope.builtin').lsp_document_symbols, '[O]pen [F]ile Symbols')
+  -- Fuzzy find all the symbols in your current workspace.
+  --  Similar to document symbols, except searches over your entire project.
+  map('gow', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[O]pen [W]orkspace Symbols')
 
-	-- Fuzzy find all the symbols in your current workspace.
-	--  Similar to document symbols, except searches over your entire project.
-	map('gow', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[O]pen [W]orkspace Symbols')
-
-	-- Jump to the type of the word under your cursor.
-	--  Useful when you're not sure what type a variable is and you want to see
-	--  the definition of its *type*, not where it was *defined*.
-	map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 end
 
 return M
--- nvim/.config/nvim/lua/plugins/code/lsp/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/code/lsp/lsp-config.lua
@@ -1,26 +1,3 @@
--- Main LSP Configuration
--- Brief aside: **What is LSP?**
---
--- LSP is an initialism you've probably heard, but might not understand what it is.
---
--- LSP stands for Language Server Protocol. It's a protocol that helps editors
--- and language tooling communicate in a standardized fashion.
---
--- In general, you have a "server" which is some tool built to understand a particular
--- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
--- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
--- processes that communicate with some "client" - in this case, Neovim!
---
--- LSP provides Neovim with features like:
---  - Go to definition
---  - Find references
---  - Autocompletion
---  - Symbol Search
---  - and more!
---
--- Thus, Language Servers are external tools that must be installed separately from
--- Neovim. This is where `mason` and related plugins come into play.
---
 -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
 -- and elegantly composed help section, `:help lsp-vs-treesitter`
 
@@ -31,10 +8,8 @@
 return {
   'neovim/nvim-lspconfig',
   dependencies = {
-    -- Useful status updates for LSP.
+    -- TODO: SEARCH IF I CAN REQUIRE FROM ANOTHER FILE INSTEAD OF JUST REFERING TO GITHUB
     { 'j-hui/fidget.nvim', opts = {} },
-
-    -- Allows extra capabilities provided by blink.cmp
     'saghen/blink.cmp',
   },
   config = function()
@@ -67,15 +42,6 @@ return {
     }
     local original_capabilities = vim.lsp.protocol.make_client_capabilities()
     local capabilities = require('blink.cmp').get_lsp_capabilities(original_capabilities)
-    -- Enable the following language servers
-    --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-    --
-    --  Add any additional override configuration in the following tables. Available keys are:
-    --  - cmd (table): Override the default command used to start the server
-    --  - filetypes (table): Override the default list of associated filetypes for the server
-    --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-    --  - settings (table): Override the default settings passed when initializing the server.
-    --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       -- Lua
       lua_ls = {
@@ -96,32 +62,14 @@ return {
       -- Python
       pyright = {},
       ruff = {},
-			
+
       -- Bash
       bashls = {},
 
       -- Docker
       docker_language_server = {},
       docker_compose_language_server = {},
-      -- rust_analyzer = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`ts_ls`) will work just fine
-      -- ts_ls = {},
-      --
     }
-
-    -- Ensure the servers and tools above are installed
-    --
-    -- To check the current status of installed tools and/or manually install
-    -- other tools, you can run
-    --    :Mason
-    --
-    -- You can press `g?` for help in this menu.
-    --
     -- `mason` had to be setup earlier: to configure its options see the
     -- `dependencies` table for `nvim-lspconfig` above.
     --
@@ -129,7 +77,7 @@ return {
     -- for you, so that they are available from within Neovim.
 
     require('mason-lspconfig').setup {
-      ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+      ensure_installed = {},
       automatic_installation = false,
       handlers = {
         function(server_name)
